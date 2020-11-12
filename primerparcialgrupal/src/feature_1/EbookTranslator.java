e90308b759e3ed05d799c53fc029e6f8595675dd
Jorge Alcivar Hurtado Duarte <jahurtado11@utpl.edu.ec>
2020-11-12 14:54:44 -0500

    Add cosntructor to the EbookTranslator class

--- primerparcialgrupal/src/feature_1/EbookTranslator.java
+++ primerparcialgrupal/src/feature_1/EbookTranslator.java
@@ -5,6 +5,10 @@ import core.EBook;
 public class EbookTranslator {
     private  ITranslate translator;
 
+    public EbookTranslator(ITranslate translator) {
+        this.translator = translator;
+    }
+
     public EBook translate(EBook ebook) {
         return translator.translate(ebook);
     }
