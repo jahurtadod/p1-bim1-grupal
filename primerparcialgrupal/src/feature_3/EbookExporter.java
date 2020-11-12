c7286e18eeb2b9ee6342c7a2ece3e8956efd1729
Jorge Alcivar Hurtado Duarte <jahurtado11@utpl.edu.ec>
2020-11-12 14:26:41 -0500

    Add methods to the EbookExporter class

--- primerparcialgrupal/src/feature_3/EbookExporter.java
+++ primerparcialgrupal/src/feature_3/EbookExporter.java
@@ -1,4 +1,19 @@
 package feature_3;
 
+import core.EBook;
+
+import java.io.File;
+
 public class EbookExporter {
+    private EBook ebook;
+    private IExportable exportable;
+
+    public EbookExporter(EBook ebook, IExportable exportable) {
+        this.ebook = ebook;
+        this.exportable = exportable;
+    }
+
+    public File exportable() {
+        return exportable.exportable(ebook);
+    }
 }
